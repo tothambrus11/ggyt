c84a4e4d65ee2a5818545f28e37073d84f76be0b
Tóth Ambrus <ambrus@ambrusweb11.hu>
2018-02-17 10:48:58 +0100

    Szép menü css

--- menu.php
+++ menu.php
@@ -1,12 +1,19 @@
+<header>
+	<h1>Óbudai Egyetem</h1>
+	<h2>Bánki Donát Gépész és Biztonságtechnikai Mérnöki Kar Gépgyártástechnológiai Szakcsoport</h2>
+</header>
 <nav>
-	<a href="<?php echo HOME_URL; ?>" class="menu-item">Kezdőlap</a>
-	<a href="<?php echo HOME_URL; ?>hirek/" class="menu-item">Hírek</a>
-	<a href="<?php echo HOME_URL; ?>tanszek/" class="menu-item">A tanszék</a>
-	<a href="<?php echo HOME_URL; ?>kepzeseink/" class="menu-item">Képzéseink</a>
-	<a href="<?php echo HOME_URL; ?>munkatarsak/" class="menu-item">Munkatársak</a>
-	<a href="<?php echo HOME_URL; ?>hallgatoknak/" class="menu-item">Hallgatóknak</a>
-	<a href="<?php echo HOME_URL; ?>szakdolgozat/" class="menu-item">Szakdolgozat</a>
-	<a href="<?php echo HOME_URL; ?>szakmai-gyakorlat/" class="menu-item">Szakmai gyakorlat</a>
-	<a href="<?php echo HOME_URL; ?>kutatasi-tevekenyseg/" class="menu-item">Kutatási tevékenység</a>
-	<a href="<?php echo HOME_URL; ?>kapcsolat/" class="menu-item">Kapcsolat gyakorlat</a>
+	<ul>
+		<li><a href="<?php echo HOME_URL; ?>" class="menu-item">Kezdőlap</a></li>
+		<li><a href="<?php echo HOME_URL; ?>hirek/" class="menu-item">Hírek</a></li>
+		<li><a href="<?php echo HOME_URL; ?>tanszek/" class="menu-item">A tanszék</a></li>
+		<li><a href="<?php echo HOME_URL; ?>kepzeseink/" class="menu-item">Képzéseink</a></li>
+		<li><a href="<?php echo HOME_URL; ?>munkatarsak/" class="menu-item">Munkatársak</a></li>
+		<li><a href="<?php echo HOME_URL; ?>hallgatoknak/" class="menu-item">Hallgatóknak</a></li>
+		<li><a href="<?php echo HOME_URL; ?>szakdolgozat/" class="menu-item">Szakdolgozat</a></li>
+		<li><a href="<?php echo HOME_URL; ?>szakmai-gyakorlat/" class="menu-item">Szakmai gyakorlat</a></li>
+		<li><a href="<?php echo HOME_URL; ?>kutatasi-tevekenyseg/" class="menu-item">Kutatási tevékenység</a></li>
+		<li><a href="<?php echo HOME_URL; ?>kapcsolat/" class="menu-item">Kapcsolat</a></li>	
+	</ul>
+	
 </nav>
