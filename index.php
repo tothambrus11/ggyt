6254a7f384c4475aeceeaa8638333011a7d3669a
Tóth Ambrus <ambrus@ambrusweb11.hu>
2018-02-17 10:13:07 +0100

    Címek, bekezdések, oldal conatiner CSS

--- index.php
+++ index.php
@@ -6,10 +6,33 @@
 <head>
 	<meta charset="utf-8">
 	<title>Kezdőlap</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 	<?php
 		include("menu.php");
 	?>
+
+	<div class="page-container">
+		<h1>Ez az oldal címe</h1>
+		<h2>Alcím</h2>
+		<p>
+			A Lorem Ipsum egy egyszerû szövegrészlete, szövegutánzata a betûszedõ és nyomdaiparnak. A Lorem Ipsum az 1500-as évek óta standard szövegrészletként szolgált az iparban; mikor egy ismeretlen nyomdász összeállította a betûkészletét és egy példa-könyvet vagy szöveget nyomott papírra, ezt használta. Nem csak 5 évszázadot élt túl, de az elektronikus betûkészleteknél is változatlanul megmaradt. Az 1960-as években népszerûsítették a Lorem Ipsum részleteket magukbafoglaló Letraset lapokkal, és legutóbb softwarekkel mint például az Aldus Pagemaker.
+
+		</p>
+		<h2>Másik alcím</h2>
+		<h3>Első vers</h3>
+		<p>
+			A Lorem Ipsum egy egyszerû szövegrészlete, szövegutánzata a betûszedõ és nyomdaiparnak. A Lorem Ipsum az 1500-as évek óta standard szövegrészletként szolgált az iparban; mikor egy ismeretlen nyomdász összeállította a betûkészletét és egy példa-könyvet vagy szöveget nyomott papírra, ezt használta. Nem csak 5 évszázadot élt túl, de az elektronikus betûkészleteknél is változatlanul megmaradt. Az 1960-as években népszerûsítették a Lorem Ipsum részleteket magukbafoglaló Letraset lapokkal, és legutóbb softwarekkel mint például az Aldus Pagemaker.
+
+		</p>
+		<h3>Másik vers</h3>
+		<p>
+			A Lorem Ipsum egy egyszerû szövegrészlete, szövegutánzata a betûszedõ és nyomdaiparnak. A Lorem Ipsum az 1500-as évek óta standard szövegrészletként szolgált az iparban; mikor egy ismeretlen nyomdász összeállította a betûkészletét és egy példa-könyvet vagy szöveget nyomott papírra, ezt használta. Nem csak 5 évszázadot élt túl, de az elektronikus betûkészleteknél is változatlanul megmaradt. Az 1960-as években népszerûsítették a Lorem Ipsum részleteket magukbafoglaló Letraset lapokkal, és legutóbb softwarekkel mint például az Aldus Pagemaker.
+
+		</p>
+	</div>
+	
+
 </body>
 </html>
